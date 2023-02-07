c362a930269bba63af7c1175e62a5269501fbce3
ponuch <ponuch.lug@gmail.com>
2023-02-07 18:51:39 +0300

    init commit

---
+++ src/main/java/org/ponuch/Main.java
@@ -0,0 +1,9 @@
+package org.ponuch;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Person name", 25, new ArrayList<>());
+    }
+}
---
+++ src/main/java/org/ponuch/Person.java
@@ -0,0 +1,31 @@
+package org.ponuch;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public final class Person {
+
+    private final String name;
+    private final int age;
+    private final Collection<String> friends;
+
+    public Person(String name, int age, Collection<String> friends) {
+        this.name = name;
+        this.age = age;
+        this.friends = new ArrayList(friends);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public Collection<String> getFriends() {
+        return Collections.unmodifiableCollection(this.friends);
+    }
+}
+
